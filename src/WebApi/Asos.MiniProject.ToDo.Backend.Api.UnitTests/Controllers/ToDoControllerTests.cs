0be1c13d22898a2c595eb70a5362a1ca3fac387c
Paul Schoolden <pauls@asos.com>
2017-09-15 12:21:56 +0100

    Made back end Api an empty shell again, moving implementation down to Session2Completed branch,

--- src/WebApi/Asos.MiniProject.ToDo.Backend.Api.UnitTests/Controllers/ToDoControllerTests.cs
+++ src/WebApi/Asos.MiniProject.ToDo.Backend.Api.UnitTests/Controllers/ToDoControllerTests.cs
@@ -1,10 +1,7 @@
 ﻿namespace Asos.MiniProject.ToDo.Backend.Api.UnitTests.Controllers
 {
-    using System.Collections.Generic;
-    using System.Web.Http.Results;
     using Asos.MiniProject.ToDo.Backend.Api.Adaptor;
     using Asos.MiniProject.ToDo.Backend.Api.Controllers;
-    using Asos.MiniProject.ToDo.Backend.Api.Models;
     using Moq;
     using NUnit.Framework;
 
@@ -24,50 +21,7 @@
         [Test]
         public void Getting_All_Items_Should_Return_All_Items()
         {
-            var items = new List<ToDoItem> { new ToDoItem(), new ToDoItem() };
-            this.todoAdapter.Setup(x => x.GetAllItemsAsync()).ReturnsAsync(items);
-
-            var result = this.toDoController.GetItems().GetAwaiter().GetResult();
-
-            Assert.That(result, Is.InstanceOf<OkNegotiatedContentResult<IEnumerable<ToDoItem>>>());
-
-            var resultItems = ((OkNegotiatedContentResult<IEnumerable<ToDoItem>>)result).Content;
-            Assert.NotNull(result);
-            Assert.That(resultItems, Is.EqualTo(items));
-        }
-
-        [Test]
-        public void Creating_An_Item_Should_Persist_A_New_Item()
-        {
-            var item = new ToDoItem();
-            item.Id = "123";
-
-            var result = this.toDoController.CreateItem(item).GetAwaiter().GetResult();
-
-            Assert.That(result, Is.InstanceOf<CreatedAtRouteNegotiatedContentResult<ToDoItem>>());
-            var resultMessage = (CreatedAtRouteNegotiatedContentResult<ToDoItem>)result;
-            Assert.That(resultMessage.RouteName, Is.EqualTo("GetItem"));
-            Assert.That(resultMessage.RouteValues["id"], Is.EqualTo(item.Id));
-            this.todoAdapter.Verify(x => x.CreateItemAsync(item), Times.Once);
-        }
-
-        [Test]
-        public void Updating_An_Item_Should_Persist_The_Update()
-        {
-            var item = new ToDoItem();
-            var id = "123";
-
-            this.toDoController.UpdateItem(item, id).GetAwaiter().GetResult();
-            this.todoAdapter.Verify(x => x.UpdateItemAsync(id, item), Times.Once);
-        }
-
-        [Test]
-        public void Deleting_An_Item_Should_Delete_It_From_The_Data_Store()
-        {
-            var id = "123";
-
-            this.toDoController.DeleteItem(id).GetAwaiter().GetResult();
-            this.todoAdapter.Verify(x => x.DeleteItemAsync(id), Times.Once);
+            Assert.Fail("Need to write a test!");
         }
     }
 }
--- src/WebApi/Asos.MiniProject.ToDo.Backend.Api/Controllers/ToDoController.cs
+++ src/WebApi/Asos.MiniProject.ToDo.Backend.Api/Controllers/ToDoController.cs
@@ -1,9 +1,7 @@
 ﻿namespace Asos.MiniProject.ToDo.Backend.Api.Controllers
 {
-    using System;
     using System.Collections.Generic;
     using System.Net;
-    using System.Threading.Tasks;
     using System.Web.Http;
     using Asos.MiniProject.ToDo.Backend.Api.Adaptor;
     using Asos.MiniProject.ToDo.Backend.Api.Models;
@@ -21,41 +19,9 @@
         [Route("todo/items")]
         [HttpGet]
         [SwaggerResponse(HttpStatusCode.OK, "Items", typeof(List<ToDoItem>))]
-        public async Task<IHttpActionResult> GetItems()
+        public IHttpActionResult GetItems()
         {
-            var items = await this.toDoItemAdaptor.GetAllItemsAsync();
-            return this.Ok(items);
-        }
-
-        [Route("todo/items", Name = "CreateItem")]
-        [HttpPost]
-        [SwaggerResponse(HttpStatusCode.OK, "Item", typeof(ToDoItem))]
-        public async Task<IHttpActionResult> CreateItem([FromBody] ToDoItem toDoItem)
-        {
-            await this.toDoItemAdaptor.CreateItemAsync(toDoItem);
-            return this.CreatedAtRoute("GetItem", new { id = toDoItem.Id }, toDoItem);
-        }
-
-        [Route("todo/items/{id}", Name = "GetItem")]
-        [HttpGet]
-        public Task<IHttpActionResult> GetItem([FromUri]string id)
-        {
-            throw new NotImplementedException("Implement in a later session.");
-        }
-
-        [Route("todo/items/{id}", Name = "UpdateItem")]
-        [HttpPatch]
-        public async Task UpdateItem([FromBody] ToDoItem toDoItem, [FromUri] string id)
-        {
-            await this.toDoItemAdaptor.UpdateItemAsync(id, toDoItem);
-        }
-
-        [Route("todo/items/{id}", Name = "DeleteItem")]
-        [HttpDelete]
-        public async Task<IHttpActionResult> DeleteItem([FromUri] string id)
-        {
-            await this.toDoItemAdaptor.DeleteItemAsync(id);
             return this.Ok();
-        }
+        }      
     }
 }
