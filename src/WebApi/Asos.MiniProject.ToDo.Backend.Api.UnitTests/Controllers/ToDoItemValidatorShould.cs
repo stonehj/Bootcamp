5048c0416803c72bb7b8c04d5624592b35ebcabe
Helen Stone <helen.stone@asos.com>
2018-05-04 11:54:37 +0100

    added validator and validator tests

---
+++ src/WebApi/Asos.MiniProject.ToDo.Backend.Api.UnitTests/Controllers/ToDoItemValidatorShould.cs
@@ -0,0 +1,40 @@
+﻿using Asos.MiniProject.ToDo.Backend.Api.Models;
+using Asos.MiniProject.ToDo.Backend.Api.Validators;
+using FluentAssert;
+using NUnit.Framework;
+using TestStack.BDDfy;
+
+namespace Asos.MiniProject.ToDo.Backend.Api.UnitTests.Controllers
+{
+    public class ToDoItemValidatorShould
+    {
+        private ToDoItem _toDoItem;
+        private bool result;
+
+        [Test]
+        public void ValidateToDoItemDescription()
+        {
+            this.Given(_ => this.GivenAToDoItemWithNoDescription())
+                .When(_ => WhenValidatingAToDoItem())
+                .Then(_ => ThenReturnFalse())
+                .BDDfy();
+        }
+
+        private void ThenReturnFalse()
+        {
+            result.ShouldBeEqualTo(false);
+        }
+
+        private void WhenValidatingAToDoItem()
+        {
+            var toDoItemValidator = new ToDoItemValidator();
+            result = toDoItemValidator.Validate(_toDoItem);
+        }
+
+        private void GivenAToDoItemWithNoDescription()
+        {
+            _toDoItem = new ToDoItem();
+            _toDoItem.Description = "";
+        }
+    }
+}
---
+++ src/WebApi/Asos.MiniProject.ToDo.Backend.Api/Validators/ToDoItemValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Asos.MiniProject.ToDo.Backend.Api.Models;
+
+namespace Asos.MiniProject.ToDo.Backend.Api.Validators
+{
+    public class ToDoItemValidator
+    {
+        public bool Validate(ToDoItem toDoItem)
+        {
+            if (toDoItem.Description == "")
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
